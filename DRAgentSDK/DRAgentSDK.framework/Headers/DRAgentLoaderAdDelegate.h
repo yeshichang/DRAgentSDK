da0e9e72e93bfbaca3816baabb7b4846975fb722
ysc <15238280098@163.com>
2018-03-22 10:43:32 +0800

    ncrease the omission callback

--- DRAgentSDK/DRAgentSDK.framework/Headers/DRAgentLoaderAdDelegate.h
+++ DRAgentSDK/DRAgentSDK.framework/Headers/DRAgentLoaderAdDelegate.h
@@ -16,18 +16,16 @@
 
 @required
 
-/**
- 广告数据加载成功
-
+/*！
+ @brief 广告数据加载成功
  @param adLoader 加载数据类
  @param adverType 广告类型
  @param advertDataModel 广告数据模型 (model)
  */
 - (void)adLoaderDidFinishLoading:(DRAgentLoader *)adLoader adverType:(IAdDataType)adverType advertDataModel:(DRAgentAdvertDataModel *)advertDataModel;
 
-/**
- 广告数据加载失败
-
+/*！
+ @brief 广告数据加载失败
  @param adLoader 加载数据类
  @param error 错误error
  */
@@ -35,12 +33,17 @@
 
 @optional
 
-/**
- webView点击返回 (从webView返回广告控制器页面)
-
+/*！
+ @brief webView点击返回 注:(有落地页，跳入webView，从webView返回广告控制器页面)
  @param adLoader 加载数据类
  */
 - (void)adLoaderWebViewReturn:(DRAgentLoader *)adLoader;
+
+/*!
+ @brief storekit中点击完成返回 注:(没有落地页，直接跳转storeVC，从storeVC返回广告控制器页面)
+ @param adLoader adLoader
+ */
+- (void)adLoaderStoreProductViewControllerReturn:(DRAgentLoader *)adLoader;
 
 @end
 
